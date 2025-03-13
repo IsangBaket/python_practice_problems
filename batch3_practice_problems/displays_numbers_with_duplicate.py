47d11c870d9c9305fa1e6736bbc3cf99b0dccd87
IsangBaket? <max.bias@myyahoo.com>
2025-03-14 07:16:35 +0800

    "first commit"

---
+++ batch3_practice_problems/displays_numbers_with_duplicate.py
@@ -0,0 +1,8 @@
+#Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry
+#start
+#input 10 numbers
+#python counts numbers
+#python counts numbers with duplicates
+#python stores first entry if numbers on one list variable
+#python prints variable with stored first entry
+#end

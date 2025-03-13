824d856c0697e59be72cd39bec96f013ebd5433a
IsangBaket? <max.bias@myyahoo.com>
2025-03-14 02:25:46 +0800

    moved_ to_another_directory

--- prints_sum_of_two_numbers.py
+++
@@ -1,8 +0,0 @@
-#Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
-print("THIS PROGRAM WILL PRINT THE SUM OF THE TWO GIVEN NUMBER")
-a = int(input("Input a number: "))
-b = int(input("Input another number: "))
-
-c = a + b
-
-print(f"the sum is {c}")

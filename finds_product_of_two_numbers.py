e42da5834d44ce4c7a3563ee0a54b215b845bf2e
IsangBaket? <max.bias@myyahoo.com>
2025-03-14 02:24:23 +0800

    moved_ to_another_directory

--- finds_product_of_two_numbers.py
+++
@@ -1,8 +0,0 @@
-#Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
-print("THIS PROGRAM WILL PRINT THE PRODUCT OF THE TWO GIVEN NUMBER")
-a = int(input("Input a number: "))
-b = int(input("Input another number: "))
-
-c = a*b
-
-print(f"the product is {c}")

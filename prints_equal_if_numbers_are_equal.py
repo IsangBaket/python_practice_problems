8feeb47eab4d70f3c5643c1d4e4afb99763726ac
IsangBaket? <max.bias@myyahoo.com>
2025-03-14 02:25:20 +0800

    moved_ to_another_directory

--- prints_equal_if_numbers_are_equal.py
+++
@@ -1,9 +0,0 @@
-#Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
-a = int(input("Input a number: "))
-b = int(input("Input another number: "))
-
-if a == b:
-    print("Equal")
-    
-else:
-    print("Not Equal")

ea56a62ddaf1c180d7c34c8139516b44a73faaad
IsangBaket? <max.bias@myyahoo.com>
2025-03-14 02:25:29 +0800

    moved_ to_another_directory

--- prints_number_of_odd_numbers_in_10_inputs.py
+++
@@ -1,9 +0,0 @@
-#Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
-odd_number_count = 0
-
-for x in range(10):
-    a = int(input("please input a number: "))
-    if a %2 != 0:
-        odd_number_count += 1
-
-print(f"The total odd number is {odd_number_count}")

3dfa75d5ec5bb8f944dde5098697f484398ac381
IsangBaket? <max.bias@myyahoo.com>
2025-03-14 19:01:08 +0800

    first commit

---
+++ batch3_practice_problems/unique_number_identifier.py
@@ -0,0 +1,10 @@
+#Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+numbers_list = []
+
+while True:
+    user_input = int(input("Input a number: "))
+    numbers_list.append(user_input)
+    if numbers_list.count() == 1:
+        print("Unique")
+    if numbers_list.count() > 1:
+        print("Duplicate")

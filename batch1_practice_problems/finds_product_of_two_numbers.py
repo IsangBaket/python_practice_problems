11dc3ac196eb48d3218261df6da41eb2eaa17869
IsangBaket? <max.bias@myyahoo.com>
2025-03-14 20:58:40 +0800

    reset directory
    problem in gitbash restarting

--- batch1_practice_problems/finds_product_of_two_numbers.py
+++
@@ -1,8 +0,0 @@
-#Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
-print("THIS PROGRAM WILL PRINT THE PRODUCT OF THE TWO GIVEN NUMBER")
-num1 = int(input("Input a number: "))
-num2 = int(input("Input another number: "))
-
-product = num1*num2
-
-print(f"the product is {product}")
--- batch1_practice_problems/finds_quotient_of_two_numbers_with_decimal.py
+++
@@ -1,8 +0,0 @@
-#Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
-print("THIS PROGRAM WILL PRINT THE QUOTIENT OF THE TWO GIVEN NUMBER")
-num1 = float(input("Input a number: "))
-num2 = float(input("Input another number: "))
-
-quotient = num1/num2
-
-print(f"{quotient} is the quotient.")

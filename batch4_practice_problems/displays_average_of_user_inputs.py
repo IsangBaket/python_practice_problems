810071d0286f36fcf9e62c8cadc59a7f47e701d3
IsangBaket? <max.bias@myyahoo.com>
2025-03-14 23:13:32 +0800

    first commit
    still on planning

---
+++ batch4_practice_problems/displays_average_of_user_inputs.py
@@ -0,0 +1,6 @@
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+
+#ask user to input numbers
+#store input numbers in a variable
+#compute average using addition and division
+#automatically compute for average when user inputs

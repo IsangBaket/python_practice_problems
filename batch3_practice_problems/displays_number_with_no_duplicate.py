c2c4d66cd7e216e641d95dc6e653d3a977b32d75
IsangBaket? <max.bias@myyahoo.com>
2025-03-14 18:36:59 +0800

    variables fixed

--- batch3_practice_problems/displays_number_with_no_duplicate.py
+++ batch3_practice_problems/displays_number_with_no_duplicate.py
@@ -1,12 +1,12 @@
 #Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
-a = []
+numbers_list = []
 
 for _ in range(10):
-    c = int(input("Input a number: "))
-    a.append(c)
+    user_input = int(input("Input a number: "))
+    numbers_list.append(user_input)
 
-for e in a:
-    if a.count(e) == 1:
-        print(e)
+for no_duplicate in numbers_list:
+    if numbers_list.count(no_duplicate) == 1:
+        print(no_duplicate)
 
 

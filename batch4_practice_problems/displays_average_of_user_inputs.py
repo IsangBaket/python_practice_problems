03ca921ac80d2ea48b91fe7293085d6e8a2b646a
IsangBaket? <max.bias@myyahoo.com>
2025-03-14 23:20:07 +0800

    found solution
    used sum and len

--- batch4_practice_problems/displays_average_of_user_inputs.py
+++ batch4_practice_problems/displays_average_of_user_inputs.py
@@ -4,3 +4,10 @@
 #store input numbers in a variable
 #compute average using addition and division
 #automatically compute for average when user inputs
+numbers = []
+
+while True:
+    user_input = int(input("Input a number: "))
+    numbers.append(user_input)
+    average = sum(numbers)/len(numbers)
+    print(average)

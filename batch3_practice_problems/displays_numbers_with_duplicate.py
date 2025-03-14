82340fb445c5c1cb2fd65d9b5f46030a7c6dffd5
IsangBaket? <max.bias@myyahoo.com>
2025-03-14 18:35:59 +0800

    first attempt

--- batch3_practice_problems/displays_numbers_with_duplicate.py
+++ batch3_practice_problems/displays_numbers_with_duplicate.py
@@ -6,3 +6,17 @@
 #python stores first entry if numbers on one list variable
 #python prints variable with stored first entry
 #end
+
+numbers_list = []
+
+for _ in range(10):
+    user_input = int(input("Input a number: "))
+    numbers_list.append(user_input)
+
+numbers_with_duplicates = []
+
+for num in numbers_list:
+    if num not in numbers_with_duplicates:
+        numbers_with_duplicates.append(num)
+        
+print(num)
